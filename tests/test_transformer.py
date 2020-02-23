3c08971eac4076c603271f32014d47de79b31f13
Abhabongse Janthong <6845502+abhabongse@users.noreply.github.com>
2020-02-24 01:58:06 +0700

    Adjust test case

--- tests/test_transformer.py
+++ tests/test_transformer.py
@@ -1,33 +1,35 @@
 from paxter.parser import Paxter
 from paxter.transformer import Transformer
 
-transformer = Transformer()
-
-INPUT_TEXT = """
-    @bold{Tom @italic{and} Jerry}!
-    My name is @name. I am @age years old!
-    @!{italic("YAS")}
-    @bold#<{##<#{}#>##}>#
-"""
-
-EXPECTED_TEXT = """
-    <b>Tom <i>and</i> Jerry</b>!
-    My name is John. I am 25 years old!
-    <i>YAS</i>
-    <b>##<#{}#>##</b>
-"""
-
-ENV = {
-    '__builtins__': {},
-    'bold': lambda token: f"<b>{token}</b>",
-    'italic': lambda token: f"<i>{token}</i>",
-    '!': lambda env, token: eval(token, env),
-    'name': "John",
-    'age': 25,
-}
-
 
 def test_base_transformer():
-    parsed_tree = Paxter.parse(INPUT_TEXT)
-    output_text = transformer.visit(ENV, parsed_tree)
-    assert output_text == EXPECTED_TEXT
+    transformer = Transformer()
+
+    input_text = """
+        @bold{Tom @italic{and} Jerry}!
+        My name is @name. I am @age years old!
+        @!{italic("YAS")}
+        @bold#<{##<#{}#>##}>#
+    """
+
+    expected_text = """
+        <b>Tom <i>and</i> Jerry</b>!
+        My name is John. I am 25 years old!
+        <i>YAS</i>
+        <b>##<#{}#>##</b>
+    """
+
+    environ = {
+        # Macros
+        '!': lambda env, token: eval(token, env),
+        # Functions
+        'bold': lambda token: f"<b>{token}</b>",
+        'italic': lambda token: f"<i>{token}</i>",
+        # Constants
+        'name': "John",
+        'age': 25,
+    }
+
+    parsed_tree = Paxter.parse(input_text)
+    output_text = transformer.visit(environ, parsed_tree)
+    assert output_text == expected_text
