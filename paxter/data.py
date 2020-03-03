8864e80bcb8ed9d00b9201fc99028fd92b24185e
Abhabongse Janthong <6845502+abhabongse@users.noreply.github.com>
2020-03-04 00:08:37 +0700

    Change data definitions to fit BNF grammar in __init__.py

--- paxter/data.py
+++ paxter/data.py
@@ -1,80 +1,136 @@
 """
 Collection of classes represents data node in parsed tree.
 """
-from dataclasses import dataclass, field
-from typing import Dict, List, Optional, Union
-
-Atom = Union[str, int, float, bool, None]
+from dataclasses import dataclass
+from typing import List, Optional, Tuple, Union
 
 
 @dataclass
-class BaseNode:
+class Node:
     """
-    Base class for all types of nodes which would appear in parsed tree.
+    Base class for all types of nodes appearing in parsed tree.
     """
-    start: int
+    start_index: int
     """Starting index of the node definition inside input string (inclusive)."""
 
-    end: int
+    end_index: int
     """Ending index of the node definition inside input string (exclusive)."""
 
 
-@dataclass
-class Fragments(BaseNode):
-    """
-    Concatenation of children nodes in the parsed tree.
-    """
-    children: List[BaseNode]
-    """List of children nodes."""
-
+#  ____                    _   _
+# | __ )  __ _ ___  ___   / \ | |_ ___  _ __ ___
+# |  _ \ / _` / __|/ _ \ / _ \| __/ _ \| '_ ` _ \
+# | |_) | (_| \__ \  __// ___ \ || (_) | | | | | |
+# |____/ \__,_|___/\___/_/   \_\__\___/|_| |_| |_|
+#
 
 @dataclass
-class RawText(BaseNode):
+class BaseAtom(Node):
     """
-    Raw text which is presented inside a list of fragments,
-    inside the @-expression macro, or embedded in `@"raw string"`.
+    Node types which are acceptable as the value part of the key-value option list.
     """
-    string: str
-    """String content."""
+    pass
 
 
 @dataclass
-class Identifier(BaseNode):
+class Identifier(BaseAtom):
     """
-    Identifier name token succeeding the @-symbol.
+    Identifier name which immediately follows the @-symbol (except for phrases)
+    or which is part of the key-value option list.
     """
     name: str
     """Identifier name."""
 
 
 @dataclass
-class AtExprMacro(BaseNode):
+class Literal(BaseAtom):
     """
-    An @-expression macro consisting of (possibly empty) identifier
-    plus the wrapped raw text (as in `@id!{raw text}`).
-
-    Note that lone `@id` that is part of a larger @-expression
-    will be expanded to `@!{id}`.
+    Literal value part of the key-value option list.
+    It can either be JSON-compatible number or string literal.
     """
-    identifier: Identifier
-    """Identifier part."""
+    value: Union[str, int, float]
+    """Python value of the JSON number of string literal."""
 
-    raw_text: RawText
-    """Raw text under the macro @-expression."""
+
+KeyValue = Tuple[Identifier, Optional[BaseAtom]]
+
+
+#  ____                 _____                                     _
+# | __ )  __ _ ___  ___|  ___| __ __ _  __ _ _ __ ___   ___ _ __ | |_
+# |  _ \ / _` / __|/ _ \ |_ | '__/ _` |/ _` | '_ ` _ \ / _ \ '_ \| __|
+# | |_) | (_| \__ \  __/  _|| | | (_| | (_| | | | | | |  __/ | | | |_
+# |____/ \__,_|___/\___|_|  |_|  \__,_|\__, |_| |_| |_|\___|_| |_|\__|
+#                                      |___/
+
+@dataclass
+class BaseFragment(Node):
+    """
+    Node types which are allowed to be part of the list of fragments.
+    """
+    pass
 
 
 @dataclass
-class AtExprFunc(BaseNode):
+class Text(BaseFragment):
     """
-    An @-expression function call consisting of the identifier,
-    the recursive fragments, and the option dictionary
-    (as in `@id[options]{fragments...}`).
+    Text which may be presented inside a list of fragments,
+    inside an @-expression macro, or embedded within the `@"raw string"`.
     """
-    identifier: Identifier
+    string: str
+    """String content."""
+
+
+@dataclass
+class PaxterMacro(BaseFragment):
+    """
+    An @-expression macro following the `@id!{raw text}` pattern,
+    consisting of an identifier (whose names ends with `!`)
+    and the wrapped text which cannot contain nested @-expressions.
+    """
+    id: Identifier
+    """Identifier part (always ending with an exclamation mark)."""
+
+    text: Text
+    """Text under the @-expression macro."""
+
+
+@dataclass
+class PaxterFunc(BaseFragment):
+    """
+    An @-expression function call following the `@id[options]{fragments...}`
+    pattern (with options) or the `@id{fragments...}` pattern (without options).
+    It consists of an identifier, the recursive fragments,
+    and the optional key-value option list.
+
+    The key-value option list is a comma-separated key-value pairs.
+    For each pair, the key part must always present as an identifier,
+    but the value part may still be absent from the key-value pair.
+    Should the value part be present, there must be a `=` sign separating
+    the key part and the value part within the key-value pair.
+    """
+    id: Identifier
     """Identifier part."""
 
-    fragments: Fragments
+    fragments: List[BaseFragment]
     """Recursively nested fragments."""
 
-    options: Dict[str, Atom] = field(default_factory=dict)
-    """Options which acts like keyword argument dictionary."""
+    options: Optional[List[KeyValue]]
+    """
+    Optional list of key-value pairs presented within the square brackets.
+    
+    - This value will be `None` when square brackets are not present.
+    - If the value part is not present within each key-value pair,
+      the value part will be represented with `None`.
+    """
+
+
+@dataclass
+class PaxterPhrase(BaseFragment):
+    """
+    An @-expression phrase is one without the identifier.
+    Normally it follows the `@{phrase}` pattern,
+    but it may also follow the simpler `@phrase` pattern
+    if it is unambiguously definitely _not_ the @-expression function call.
+    """
+    phrase: Text
+    """Any text phrase."""
