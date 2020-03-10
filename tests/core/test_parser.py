5bc92d121ced9f444a5cbf29110a1f4dce6fdc32
Abhabongse Janthong <6845502+abhabongse@users.noreply.github.com>
2020-03-10 10:09:42 +0700

    Add more parsing unit tests

--- tests/core/test_parser.py
+++ tests/core/test_parser.py
@@ -175,6 +175,127 @@ from paxter.core import (FragmentList, Identifier, Parser, PaxterFunc, PaxterMac
                 ],
             ),
         ),
+        pytest.param(
+            r'Hello @good boy @{good} boy @<{good boy}>',
+            FragmentList(
+                start_pos=0, end_pos=41,
+                children=[
+                    Text(start_pos=0, end_pos=6, string="Hello "),
+                    PaxterPhrase(
+                        start_pos=6, end_pos=11,
+                        phrase=Text(start_pos=7, end_pos=11, string="good"),
+                    ),
+                    Text(start_pos=11, end_pos=16, string=" boy "),
+                    PaxterPhrase(
+                        start_pos=16, end_pos=23,
+                        phrase=Text(start_pos=18, end_pos=22, string="good"),
+                    ),
+                    Text(start_pos=23, end_pos=28, string=" boy "),
+                    PaxterPhrase(
+                        start_pos=28, end_pos=41,
+                        phrase=Text(start_pos=31, end_pos=39, string="good boy"),
+                    ),
+                ],
+            ),
+        ),
+        pytest.param(
+            r'@hello{@hello#{@hello<{@hello and {}}>{}}#}',
+            FragmentList(
+                start_pos=0, end_pos=43,
+                children=[
+                    PaxterFunc(
+                        start_pos=0, end_pos=43,
+                        id=Identifier(start_pos=1, end_pos=6, name="hello"),
+                        fragments=FragmentList(
+                            start_pos=7, end_pos=42,
+                            children=[
+                                PaxterFunc(
+                                    start_pos=7, end_pos=42,
+                                    id=Identifier(
+                                        start_pos=8, end_pos=13,
+                                        name="hello",
+                                    ),
+                                    fragments=FragmentList(
+                                        start_pos=15, end_pos=40,
+                                        children=[
+                                            PaxterFunc(
+                                                start_pos=15, end_pos=38,
+                                                id=Identifier(
+                                                    start_pos=16, end_pos=21,
+                                                    name="hello",
+                                                ),
+                                                fragments=FragmentList(
+                                                    start_pos=23,
+                                                    end_pos=36,
+                                                    children=[
+                                                        PaxterPhrase(
+                                                            start_pos=23,
+                                                            end_pos=29,
+                                                            phrase=Text(
+                                                                start_pos=24,
+                                                                end_pos=29,
+                                                                string="hello",
+                                                            ),
+                                                        ),
+                                                        Text(
+                                                            start_pos=29, end_pos=36,
+                                                            string=" and {}",
+                                                        ),
+                                                    ],
+                                                ),
+                                                options=None,
+                                            ),
+                                            Text(start_pos=38, end_pos=40, string="{}"),
+                                        ],
+                                    ),
+                                    options=None,
+                                ),
+                            ],
+                        ),
+                        options=None,
+                    ),
+                ],
+            ),
+        ),
+        pytest.param(
+            r'@hello{} @hello[]{} @hello[x]{} @hello[x,]{}',
+            FragmentList(
+                start_pos=0, end_pos=44,
+                children=[
+                    PaxterFunc(
+                        start_pos=0, end_pos=8,
+                        id=Identifier(start_pos=1, end_pos=6, name="hello"),
+                        fragments=FragmentList(start_pos=7, end_pos=7, children=[]),
+                        options=None,
+                    ),
+                    Text(start_pos=8, end_pos=9, string=" "),
+                    PaxterFunc(
+                        start_pos=9, end_pos=19,
+                        id=Identifier(start_pos=10, end_pos=15, name="hello"),
+                        fragments=FragmentList(start_pos=18, end_pos=18, children=[]),
+                        options=[],
+                    ),
+                    Text(start_pos=19, end_pos=20, string=" "),
+                    PaxterFunc(
+                        start_pos=20, end_pos=31,
+                        id=Identifier(start_pos=21, end_pos=26, name="hello"),
+                        fragments=FragmentList(start_pos=30, end_pos=30, children=[]),
+                        options=[
+                            (Identifier(start_pos=27, end_pos=28, name="x"), None),
+                        ],
+                    ),
+                    Text(start_pos=31, end_pos=32, string=" "),
+                    PaxterFunc(
+                        start_pos=32, end_pos=44,
+                        id=Identifier(start_pos=33, end_pos=38, name="hello"),
+                        fragments=FragmentList(start_pos=43, end_pos=43, children=[]),
+                        options=[
+                            (Identifier(start_pos=39, end_pos=40, name="x"), None),
+                        ],
+                    ),
+                ],
+            ),
+        ),
     ],
 )
 def test_parser(input_text, expected):
