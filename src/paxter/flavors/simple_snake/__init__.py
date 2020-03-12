a8d9e08afd815b4f84f19e947260325169578f3f
Abhabongse Janthong <6845502+abhabongse@users.noreply.github.com>
2020-03-12 07:52:16 +0700

    simple snake: Add more documentation on if/for

--- src/paxter/flavors/simple_snake/__init__.py
+++ src/paxter/flavors/simple_snake/__init__.py
@@ -33,15 +33,15 @@ using the following data in this order
 
 3.  These following macros:
 
-    - The single `!` macro which simply executes the main text input as python code.
-      To print to the output text at the same location,
-      the code must write to the `StringIO` object called `buffer`
-      which will be temporarily be injected into each `!` macro call.
-      [See examples below.](#buffer-example)
+    -   The single `!` macro which simply executes the main text input as python code.
+        To print to the output text at the same location,
+        the code must write to the `StringIO` object called `buffer`
+        which will be temporarily be injected into each `!` macro call.
+        [See examples below.](#buffer-example)
 
-    - The `load!` macro which loads a set of extra functions into the environment.
-      Available sets are `string`, `html`, and `base64`.
-      See more at `paxter.flavors.simple_snake.functions`.
+    -   The `load!` macro which loads a set of extra functions into the environment.
+        Available sets are `string`, `html`, and `base64`.
+        See more at `paxter.flavors.simple_snake.functions`.
 
 4.  Starting environment dict `start_env` provided at the construction
     of the `paxter.flavors.simple_snake.SimpleSnakeTransformer` instance.
@@ -51,16 +51,36 @@ using the following data in this order
 
 6.  Any side-effects happen during the interpretation of the input text.
 
+#### Notes
+
+-   Values with identifier matching python keywords might not be visible in the environment.
+
+#### Special patterns
+
+-   Paxter function expression `@if[test,true]{...}` or `@if[test,false]{...}`
+    is a special pattern in Simple Snake:
+    the value with id `test` from inside the environment will be tested for truthiness.
+    If it does indeed match the target boolean (provided as the second argument)
+    then the main text would have then been evaluated and used.
+    Otherwise, the main text would not be evaluated and has no effect.
+
+    The target boolean (i.e. the second argument) must either be `true` or `false`.
+    It can be omitted like `@if[test]{...}`;
+    in such case, the target `true` will be assumed.
+
+-   Paxter function expression `@for[item_id,seq]{...}`
+    is a special pattern in Simple Snake:
+    the id `item_id` will bind to each item in `seq`
+    which will appear in the environment while evaluating the main text.
+    The main text will be repeatedly evaluated for each new item in `seq`,
+    and the final result will be the concatenation of evaluated main texts
+    from each loop iteration.
+
 ### Post Processing
 
 At the end of the transformation, if a line ends with a backslash,
 then the backslash itself along with the newline character next to it
 will be removed from the output text.
-
-## Example
-
-### Buffer Example
-
 
 """
 from paxter.core.transformer import visitor_method_names
