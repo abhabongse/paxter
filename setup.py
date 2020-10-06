dc9fbdefd6b7a5dd4ce2f5c515b6aab78e6bb746
Abhabongse Janthong <6845502+abhabongse@users.noreply.github.com>
2020-10-07 01:45:38 +0700

    Bump support for python3.9 in setup.py

--- setup.py
+++ setup.py
@@ -40,6 +40,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Topic :: Software Development :: Pre-processors',
         'Topic :: Text Processing',
         'Typing :: Typed',
